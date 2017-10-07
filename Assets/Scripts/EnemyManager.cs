13e782782660e3e53894d1643afc335070707c27
jakub_desktop <pay64k@gmail.com>
2017-10-07 12:34:08 +0200

    init

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+    public int maxEnemies;
+
+    [SerializeField]
+    private List<GameObject> enemies;
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        enemies = new List<GameObject>();
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if (enemies.Count >= maxEnemies)
+        {
+            return;
+        }
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        GameObject enemy = Instantiate(enemyPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        enemy.transform.parent = gameObject.transform;
+        enemies.Add(enemy);
+
+    }
+
+    public void enemyKilled(GameObject enemy)
+    {
+        enemies.Remove(enemy);
+    }
+}
---
+++ Assets/Scripts/Progress.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Progress : MonoBehaviour
+{
+
+    Image foregroundImage;
+
+    public int Value
+    {
+        get
+        {
+            if (foregroundImage != null)
+                return (int)(foregroundImage.fillAmount * 100);
+            else
+                return 0;
+        }
+        set
+        {
+            if (foregroundImage != null)
+                foregroundImage.fillAmount = value / 100f;
+        }
+    }
+
+    void Start()
+    {
+        foregroundImage = gameObject.GetComponent<Image>();
+        Value = 0;
+    }
+    
+    //Testing: this function will be called when Test Button is clicked
+    public void UpdateProgress(float reloadTime)
+    {
+        Hashtable param = new Hashtable();
+        param.Add("from", 0.0f);
+        param.Add("to", 100);
+        param.Add("time", reloadTime);
+        param.Add("onupdate", "TweenedSomeValue");
+        param.Add("onComplete", "OnFullProgress");
+        param.Add("onCompleteTarget", gameObject);
+        iTween.ValueTo(gameObject, param);
+    }
+
+    public void TweenedSomeValue(int val)
+    {
+        Value = val;
+    }
+
+    public void OnFullProgress()
+    {
+        Value = 0;
+    }
+}
